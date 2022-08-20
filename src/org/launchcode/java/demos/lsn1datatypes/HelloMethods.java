85032af5ec039ae2a168604feaa3cfb5ee3c50da
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-20 16:26:45 -0500

    Added choice to HelloMethods.java

--- src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
+++ src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
@@ -1,10 +1,16 @@
 package org.launchcode.java.demos.lsn1datatypes;
+import java.util.Scanner;
 
 public class HelloMethods {
 
     public static void main(String[] args) {
-        String message = Message.getMessage("fr");
-        System.out.println(message);
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Please enter language preference (fr = French, sp = Spanish, ger = German, eng = English): ");
+        String choice = input.next(); //Declares the 'name' variable and initializes it with text from the command line.
+        input.close();
+        //String message = Message.getMessage("fr");
+        System.out.println(Message.getMessage(choice));
     }
 
 }
