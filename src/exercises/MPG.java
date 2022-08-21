2a7b522285e36d62ac7ab5467c0e4f2fc97373c4
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-20 19:16:05 -0500

    finished MPG

--- src/exercises/MPG.java
+++ src/exercises/MPG.java
@@ -1,4 +1,15 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class MPG {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven today?");
+        int miles = input.nextInt();
+        System.out.println("How many gallons of gas have you consumed?");
+        int gallons = input.nextInt();
+        int milesPerGallon = miles/gallons;
+        System.out.println("You have achived a total MPG of " +milesPerGallon +" MPG today." );
+    }
 }
