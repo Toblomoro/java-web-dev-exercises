c618adbe6452ccbfedeb95b5c0e48dc669f0111e
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-20 19:05:17 -0500

    workign on AreaCalculator.java

---
+++ src/exercises/AreaCalculator.java
@@ -0,0 +1,14 @@
+package exercises;
+import java.util.Scanner;
+
+public class AreaCalculator {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length?");
+        int length = input.nextInt();
+        System.out.println("What is the width?");
+        int width = input.nextInt();
+
+
+    }
+}
