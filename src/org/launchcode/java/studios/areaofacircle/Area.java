a2093ee4120869a5c1e852e91f760c94082f61d9
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-22 20:06:12 -0500

    tried but couldnt string check

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -2,19 +2,24 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        while (radius < 0){
+        Double radius = input.nextDouble();
+        String alpha = input.next();
+
+        while (radius < 0) {
             System.out.println("enter a positive number for radius");
             radius = input.nextDouble();
-        } if (radius > 0){
+        }
+        if (radius > 0) {
             double area = Circle.getArea(radius);
-            System.out.println(" The circle's area is "+area ) ;
+            System.out.println(" The circle's area is " + area);
             input.close();
         }
-
-
+        if (radius = char){
+            System.out.println("enter a number for radius");
+            radius = input.nextDouble();
+        }
     }
 }
