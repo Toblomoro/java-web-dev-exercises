5cc6b627ee5fd39d5ac6c9c140ca76042d270f94
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-24 17:22:47 -0500

    finsihed sentence array on to Array_list_practice

---
+++ src/exercises/Array_list_practice.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class Array_list_practice {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/exercises/Array_string_practice.java
@@ -0,0 +1,12 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class Array_string_practice {
+    public static void main(String[] args) {
+        String greenEggsAndHam = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+        String[] splitEggsAndHam = greenEggsAndHam.split("\\.");
+        System.out.println(Arrays.toString(splitEggsAndHam));
+    }
+}
