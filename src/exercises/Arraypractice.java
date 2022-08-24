a03d4dcfd922894110c6f8e33862dba17ab037d8
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-24 17:05:16 -0500

    Moved to exercises finished loop in array

---
+++ src/exercises/Arraypractice.java
@@ -0,0 +1,15 @@
+package exercises;
+
+public class Arraypractice {
+    public static void main(String[] args){
+        int[] someArray ={ 1, 1, 2, 3, 5, 8};
+        for (int i: someArray ) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
