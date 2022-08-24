618e47fed78c217ba56f166336434908a848ad19
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-24 17:08:17 -0500

    Moved to exercises finished odd loop in array

--- src/exercises/Arraypractice.java
+++ src/exercises/Arraypractice.java
@@ -1,15 +1,14 @@
 package exercises;
 
 public class Arraypractice {
-    public static void main(String[] args){
-        int[] someArray ={ 1, 1, 2, 3, 5, 8};
-        for (int i: someArray ) {
-            System.out.println(i);
+    public static void main(String[] args) {
+        int[] someArray = {1, 1, 2, 3, 5, 8};
+        for (int i : someArray) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+
+
         }
-
-
-
-
-
     }
 }
