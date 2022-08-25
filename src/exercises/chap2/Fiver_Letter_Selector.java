d46049e1bcafd13e5f4617f3fa8bcf303767d10b
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-24 20:38:14 -0500

    completed with scanner

--- src/exercises/chap2/Fiver_Letter_Selector.java
+++ src/exercises/chap2/Fiver_Letter_Selector.java
@@ -1,22 +1,34 @@
 package exercises.chap2;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Fiver_Letter_Selector {
-    static ArrayList<String> wordList = new ArrayList<String>(
+    static ArrayList<String> wordList = new ArrayList<>(
             Arrays.asList("Hello", "I", "enjoy", "frolicking", "through", "olive", "groves")
     );
     public static void main(String[] args) {
-       for (String word : wordList){
-           if (word.length() == 5){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many letters would you like to search for?");
+        Integer length = input.nextInt();
+
+        /*int width = input.nextInt();
+        int area = length*width;
+        System.out.println(" The rectangle's area is " +area );
+        input.close();*/
+
+
+
+
+        for (String word : wordList){
+           if (word.length() == length){
                System.out.println(word);
 
            }
        }
     }
-
-};
+}
 
 
 
