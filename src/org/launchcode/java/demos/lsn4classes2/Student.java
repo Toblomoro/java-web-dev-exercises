04ee9f66118499fc7d3ad93b932b165618a3c3fa
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-31 21:34:50 -0500

    exercises chap 4 working but math is incorrect

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -7,6 +7,10 @@ public class Student {
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
+    private String gradeLevel;
+
+
+
 
     public Student (String name, int studentId, int numberOfCredits, double gpa) {
         this.name = name;
@@ -30,20 +34,86 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+  public String getGradeLevel() {
+      if (numberOfCredits <= 20 ) {
+          return "Freshman";
+      }
+      if (numberOfCredits <= 40) {
+          return "Sophomore";
+      }
+      if (numberOfCredits <= 60) {
+          return "Junior";
+      }
+      if (numberOfCredits <= 80) {
+          return "Senior";
+      }
+
+
+ // Determine the grade level of the student based on numberOfCredits
+      return gradeLevel;
+ }
 
     // TODO: Complete the addGrade method.
-    public void addGrade(int courseCredits, double grade) {
-        // Update the appropriate fields: numberOfCredits, gpa
-    }
+    public void addGrade(double courseCredits, double grade) {
+        //Update the appropriate fields: numberOfCredits, gpa
+        //if number of credits is 4 then A, 3 then B, 2 then C, 1 then D, 0 then fail.
+        //quality score = (letter grade * course credits) finalGPA = (quality score /total credits
+        Double quality = gpa * numberOfCredits;
+        Double newQuality = courseCredits * grade;
+        Double updatedQuality = quality + newQuality;
+        Double updatedCredits = courseCredits + numberOfCredits;
+        Double numberOfCredits= updatedCredits;
+        gpa = quality/numberOfCredits;
+
+      /*  if (gpa >= 3.0) {
+            grade = "A";
+        }
+
+        if (gpa >= 2.0 || gpa <= 3.0) {
+            grade = "B";
+        }
+        if (gpa >= 1.0 || gpa <= 2.0) {
+            grade ="C";
+        }
+        if (gpa >= 0.0 || gpa <= 1.0) {
+            grade = "D";
+        }
+        if (gpa <= 0.0) {
+            grade = "F";
+        }*/
+
+        }
+
+
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    public String toString() {
+        return name + " (Credits: " + numberOfCredits + ", GPA: " + gpa + ")";
+    }
+
+
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+    public boolean equals(Object toBeCompared) {
+
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Student theStudent = (Student) toBeCompared;
+        return theStudent.getStudentId() == getStudentId();
+    }
+
 
     public String getName() {
         return name;
